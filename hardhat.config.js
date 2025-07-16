c7464ccb0895320733ca178d2542402acb9fb77c
macuih <macuihlein2025@u.northwestern.edu>
2025-07-16 13:03:48 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,20 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-paths: {
-    artifacts: "./src/artifacts",
+  paths: {
+    artifacts: "./src/artifacts", 
   },
-        networks: {
-                localhost: {
-                        url: "http://ec2-3-144-201-231.us-east-2.compute.amazonaws.com:8545"
-            ,           chainID: 1337
-
-                }
-,
-                hardhat: {
-                        chainID: 1337
-           }
-   },
-
-        solidity: "0.8.18",
+  networks: {
+    localhost: {
+      url: "http://ec2-3-144-201-231.us-east-2.compute.amazonaws.com:8545", 
+      chainId: 1337, 
+    },
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  solidity: "0.8.18",
 };
